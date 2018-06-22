cdc3e6aaf88214e39045c91ec536fc2aee7ed451
Rehlaender <mario.rehlaender@accenture.com>
2018-06-22 17:34:50 -0500

    can now change colors and needs actions for buttons

--- src/components/Drawing.js
+++ src/components/Drawing.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
-import Canvas from 'simple-react-canvas';
-import CanvasDraw from "react-canvas-draw";
 import MyCanvasDraw from './MyCanvasDraw';
 import { publishLine, subscribeToDrawingLines } from '../api';
+import { HuePicker, AlphaPicker } from 'react-color'
 
 class Drawing extends Component {
   state = {
     lines: [],
+    color: '#000',
   }
+
+
 
   componentDidMount() {
     subscribeToDrawingLines(this.props.drawing.id, (linesEvent) => {
@@ -37,16 +39,29 @@ class Drawing extends Component {
     let windowWidth = window.innerWidth-20;
     let windowHeigth = window.innerHeight-20;
 
+    const handleColorChange = ({ hex }) => this.setState({color: hex})
+
     return (this.props.drawing) ? (
       <div
         className="Drawing"
       >
         <div className="Drawing-title">{this.props.drawing.name} ({this.state.lines.length} lines)</div>
+        <div className="controlers">
+          <HuePicker
+            onChangeComplete={handleColorChange}
+            color={this.state.color}
+            />
+        </div>
+        <div className="drawingButtons">
+          <div className="actionButton" style={{backgroundColor: '#BAF4FF'}}><i class="fas fa-chevron-left"></i></div>
+          <div className="actionButton" style={{backgroundColor: 'tomato'}}><i class="fas fa-trash-alt"></i></div>
+        </div>
         <MyCanvasDraw
           lines={this.state.lines}
           onChange={this.handleDraw}
           canvasWidth={windowWidth}
           canvasHeight={windowHeigth}
+          brushColor={this.state.color}
         />
       </div>
     ) : null;
